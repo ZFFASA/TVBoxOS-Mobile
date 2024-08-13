0c4a5a95131fa83cf3198e24b7adb6e0ed10a55c
ZFFASA <649019350@qq.com>
2024-08-14 04:59:10 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -128,7 +128,8 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "
+http://yezilive.jxpan.vip:82/iptv.txt");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
